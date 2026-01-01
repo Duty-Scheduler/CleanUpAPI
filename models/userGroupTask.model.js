c85bff2b8a62835519da9128169eb7282afde7ea
Your Name <you@example.com>
2026-01-01 20:36:54 +0700

    style: format code for consistency in UserGroupTask model

--- models/userGroupTask.model.js
+++ models/userGroupTask.model.js
@@ -1,48 +1,50 @@
-import { DataTypes } from 'sequelize';
-import sequelize from '../lib/db.js';
-import Penalty from './penalty.model.js';
+import { DataTypes } from "sequelize";
+import sequelize from "../lib/db.js";
+import Penalty from "./penalty.model.js";
 
-const UserGroupTask  = sequelize.define('UserGroupTask', {
+const UserGroupTask = sequelize.define(
+  "UserGroupTask",
+  {
     id: {
-        type: DataTypes.UUID,
-        defaultValue: DataTypes.UUIDV4,
-        primaryKey: true,
-        allowNull: false,
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+      allowNull: false,
     },
     penalty_status: {
-        type: DataTypes.BOOLEAN,
-        defaultValue: false,
-        allowNull: false,
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+      allowNull: false,
     },
     isAdmin: {
-        type: DataTypes.BOOLEAN,
-        defaultValue: false,
-        allowNull: false
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+      allowNull: false,
     },
     UserId: {
-        type: DataTypes.UUID,
-        allowNull: false,
-        unique: false
+      type: DataTypes.UUID,
+      allowNull: false,
+      unique: false,
     },
     GroupId: {
-        type: DataTypes.UUID,
-        allowNull: false,
-        unique: false
+      type: DataTypes.UUID,
+      allowNull: false,
+      unique: false,
     },
     TaskId: {
-        type: DataTypes.UUID,
-        allowNull: true,
-        unique: false
-    }
-    }, {
+      type: DataTypes.UUID,
+      allowNull: true,
+      unique: false,
+    },
+  },
+  {
     indexes: [
-        {
+      {
         unique: true,
-        fields: ["UserId", "GroupId", "TaskId"]
-        }
-    ]
-});
+        fields: ["UserId", "GroupId", "TaskId"],
+      },
+    ],
+  }
+);
 
-
-
-export default UserGroupTask ;
+export default UserGroupTask;
